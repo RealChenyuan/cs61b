fb834ee750ce01f93484e2d11b9d92312268a17f
RealChenyuan <chenyan29@mail2.sysu.edu.cn>
2023-03-27 15:28:29 +0800

    hw1-v1

---
+++ hw1/HW1/src/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,22 @@
+package synthesizer;
+
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T>{
+    protected int fillCount;
+    protected int capacity;
+    public int capacity() {
+        return capacity;
+    }
+    public int fillCount() {
+        return fillCount;
+
+    }
+//    public boolean isEmpty() {// is the buffer empty (fillCount equals zero)?
+//        return fillCount() == 0;
+//    }
+//    public boolean isFull() { // is the buffer full (fillCount is same as capacity)?
+//        return fillCount() == capacity();
+//    }
+    public abstract T peek();
+    public abstract T dequeue();
+    public abstract void enqueue(T x);
+}
